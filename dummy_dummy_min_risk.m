dcea073244c9e4507bf51e0e0aaad4886587bc1f
Julien Aigret <julien.aigret@student.uclouvain.be>
2016-05-11 00:49:35 +0200

    MAJ fonctions MATLAB

---
+++ dummy_dummy_min_risk.m
@@ -0,0 +1,2 @@
+function omega = dummy_dummy_min_risk(esp, cov, max_budget)
+omega = zeros(size(esp));
--- dummy_min_risk.m
+++ dummy_min_risk.m
@@ -1,14 +1,15 @@
 function omega = dummy_min_risk(esp, cov, max_budget)
 omega = zeros(size(esp));
-max = size(cov);
-min = Inf;
-ind = 0;
-for i 1:max
-    if cov(i,i) < min;
-        min = cov (i,i);
-        ind = i;
+max = size(cov,1);
+i = 1;
+hit = max;
+for i = 1:max
+    if cov(i,i) ~= 0
+        esp (i) = 0;
+        hit --;
     end
 end
-omega(ind) = max_budget;
-
-    
+if (hit ~= 0)
+    [val, i] = max(esp);
+    omega(i) = max_budget;
+end
